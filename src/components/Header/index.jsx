3671ae390ae590a8cd158a8f6557ae0b7b46859b
Yejj <1271361907@qq.com>
2019-04-18 23:39:18 +0800

    first

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {Row,Col} from 'antd';
+import './index.less'
+import Util from '../../utils/utils'
+
+export default class Header extends React.Component{
+    state={
+
+    }
+    componentWillMount(){
+        this.setState({
+            userName:'NightJson'
+        })
+        setInterval(()=>{
+           let sysTime =  Util.formateDate(new Date().getTime());
+           this.setState({
+               sysTime
+           })
+        },100)
+    }
+    render() {
+        return (
+            <div className="header">
+               <Row className="header-top">
+                   <Col span={24}>
+                         <span>欢迎,{this.state.userName}</span>
+                         <a href="#">退出</a>
+                   </Col>   
+                </Row>   
+                <Row className="breadercrumb">
+                    <Col span={4} className="breadercrumb-title">
+                        首页
+                    </Col>
+                    <Col span={20} className="weather">
+                        <span className="date">{this.state.sysTime}</span>
+                        <span className="weather-detail">晴转多云</span>
+                    </Col>
+                </Row> 
+             </div>
+        )
+    }
+}
--- src/pages/demo/Life.js
+++ src/pages/demo/Life.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Child from './Child'
+import {Button} from 'antd'
+
+import './index.less'
+
 export default class Life extends React.Component{
     constructor(props) {
         super(props);
@@ -18,8 +22,9 @@ export default class Life extends React.Component{
         })
     }
     render() {
-        return <div>
+        return <div className="content">
             <p>react生命周期介绍</p>
+            <Button onClick={this.handleAdd}>点击一下</Button>
             <button onClick={this.handleAdd}>点击一下</button>
             <button onClick={this.handleClick.bind(this)}>点击一下</button>
             <p>{this.state.count}</p>
