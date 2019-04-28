37a1d49421a3532413e99edf2214a09ccfb04854
Yejj <1271361907@qq.com>
2019-04-28 23:43:06 +0800

    map

--- src/pages/order/detail.jsx
+++ src/pages/order/detail.jsx
@@ -26,13 +26,82 @@ export default class OrderDetail extends React.Component{
                 this.setState({
                     orderInfo:res.result
                 })
+                this.renderMap(res.result)
          
             }
         })
     }
 
-    renderMap = ()=>{
+    renderMap = (result)=>{
+        this.map = new window.BMap.Map('orderDetailMap');
+        //坐标点
+        // this.map.centerAndZoom('北京',11);
+        //控件
+        this.addMapControl();
+        //路线图绘制
+      
+        this.drawBikeRoute(result.position_list);
+        this.drwaServiceArea(result.area);
         
+    }
+    addMapControl = ()=>{
+        let map = this.map;
+        map.addControl(new window.BMap.ScaleControl({ anchor: window.BMAP_ANCHOR_TOP_RIGHT}));
+        map.addControl(new window.BMap.NavigationControl({ anchor: window.BMAP_ANCHOR_TOP_RIGHT }));
+    }
+    drawBikeRoute = (positionList)=>{
+        let map = this.map;
+        let startPoint = '';
+        let endPoint = '';
+        if(positionList.length>0){
+            let arr = positionList[0];
+            let last = positionList[positionList.length-1];
+            startPoint = new window.BMap.Point(arr.lon,arr.lat);
+            let startIcon = new window.BMap.Icon('/assets/start_point.png',new window.BMap.Size(36,42),{
+                imageSize:new window.BMap.Size(36,42),
+                anchor: new window.BMap.Size(18, 42)
+            })
+            let startMarker = new window.BMap.Marker(startPoint,{icon:startIcon})
+            this.map.addOverlay(startMarker);
+
+            endPoint = new window.BMap.Point(last.lon, last.lat);
+            let endIcon = new window.BMap.Icon('/assets/end_point.png', new window.BMap.Size(36, 42), {
+                imageSize: new window.BMap.Size(36, 42),
+                anchor: new window.BMap.Size(18, 42)
+            })
+            let endMarker = new window.BMap.Marker(endPoint, { icon: endIcon });
+            this.map.addOverlay(endMarker);
+            //路线图
+            let trackPoint = [];
+            for(let i=0;i<positionList.length;i++){
+                let point = positionList[i];
+                trackPoint.push(new window.BMap.Point(point.lon, point.lat));
+            }
+            let polyline = new window.BMap.Polyline(trackPoint,{
+                strokeColor:'#1869AD',
+                strokeWeight:3,
+                strokeOpacity:1
+            })
+            this.map.addOverlay(polyline);
+            this.map.centerAndZoom(endPoint, 11);
+        }
+        
+    }
+    drwaServiceArea =(positionList)=>{
+        let trackPoint = [];
+        for (let i = 0; i < positionList.length; i++) {
+            let point = positionList[i];
+            trackPoint.push(new window.BMap.Point(point.lon, point.lat));
+        }
+        // 绘制服务区
+        let polygon = new window.BMap.Polygon(trackPoint, {
+            strokeColor: '#CE0000',
+            strokeWeight: 4,
+            strokeOpacity: 1,
+            fillColor: '#ff8605',
+            fillOpacity:0.4
+        })
+        this.map.addOverlay(polygon);
     }
     render(){
         const info = this.state.orderInfo || {};
