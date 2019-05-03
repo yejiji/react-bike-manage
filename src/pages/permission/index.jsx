05a4a1973e93750a90382599c74c63bb633dafaa
Yejj <1271361907@qq.com>
2019-05-03 22:00:57 +0800

    per

---
+++ src/pages/permission/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Card, Button, Form, Input, Select, Tree, Transfer, Modal} from 'antd'
+import axios from '../../axios/index'
+import ETable from '../../components/ETable/index'
+import menuConfig from '../../config/menuConfig'
+import Utils from '../../utils/utils'
+const FormItem = Form.Item;
+const Option = Select.Option;
+const TreeNode = Tree.TreeNode;
+export default class Order extends React.Component{
+
+    
+}
